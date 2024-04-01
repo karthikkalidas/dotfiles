a0a362a992864b881a6ccc77188700ccf29bffee
karthikkalidas <karthik.kalidas@gmail.com>
2024-04-01 20:40:21 +0530

    first commit

---
+++ nvim/lua/custom/mappings.lua
@@ -0,0 +1,65 @@
+local M = {}
+local opt = vim.opt
+
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = {
+      "<cmd> DapToggleBreakpoint <CR>",
+      "Add breakpoint at line"
+    },
+    ["<leader>dr"] = {
+      "<cmd> DapContinue <CR>",
+      "Start or continue the debugger"
+    },
+    ["<leader>dus"] = {
+      function()
+        local widgets = require('dap.ui.widgets');
+        local sidebar = widgets.sidebar(widgets.scopes);
+        sidebar.open();
+      end,
+      "Open debugging sidebar"
+    }
+  }
+}
+
+M.dap_go = {
+  plugin = true,
+  n = {
+    ["<leader>dgt"] = {
+      function()
+        require('dap-go').debug_test()
+      end,
+      "Debug go test"
+    },
+    ["<leader>dgl"] = {
+      function()
+        require('dap-go').debug_last()
+      end,
+      "Debug last go test"
+    }
+  }
+}
+
+M.dap_python = {
+  plugin = true,
+  n = {
+    ["<leader>dpr"] = {
+      function()
+        require('dap-python').test_method()
+      end,
+    }
+  }
+}
+
+M.miscellaneous = {
+  n = {
+    ["<leader>gg"] = { "<cmd> LazyGit <CR>", "Open LazyGit" },
+  }
+}
+
+opt.relativenumber = true
+opt.number = true
+
+return M
+
