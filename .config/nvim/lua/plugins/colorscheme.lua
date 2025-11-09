2af84c62fbe7e4374fda25f7d9923c07e4b70694
Karthik Kalidas <karthik.kalidas@gmail.com>
2025-11-09 12:32:24 -0600

    colorscheme

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  { "dracula/vim" },
+  { "catppuccin/nvim", name = "catppuccin" },
+  { "rebelot/kanagawa.nvim", name = "kanagawa" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
